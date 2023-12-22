e704ef9496320e56a7ffccbc1b2c9fd6090538e0
Niel Pagupat <niel.pagupat.2016@gmail.com>
2023-12-22 11:51:56 +0800

    Login2

--- Components/Login.js
+++ Components/Login.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Card, Text, TextInput, Button } from 'react-native-paper'
+import { Card, Text, TextInput, Button, Avatar } from 'react-native-paper'
 import { StyleSheet, View } from 'react-native'
 
 export default function Login() {
+    const [pVisibility, setPVisibility] = useState(true)
+    const [eyeIcon, setIcon] = useState('eye-off')
+    const showPass = () => {
+        if (pVisibility == true) {
+            setPVisibility(false)
+            setIcon('eye')
+        } else {
+            setPVisibility(true)
+            setIcon('eye-off')
+        }
+    }
   return (
     <SafeAreaView style={styles.Content}>
+        <Avatar.Image size={100} source={require('../assets/StoreIO_Logo.png')} style={{marginBottom:50, backgroundColor:'rgba(0,0,0,0)'}}/>
         <View style={styles.view}>
             <Card>
                 <Card.Title title = "Inventoraku" titleStyle={styles.title}></Card.Title>
                 <Card.Content>
                     <TextInput label= "email" keyboardType='email-address'></TextInput>
-                    <TextInput label="password" secureTextEntry={true}></TextInput>
-                    <Button>Forgot password?</Button>
+                    <TextInput label="password" secureTextEntry={pVisibility} style={{marginTop:10}} right={<TextInput.Icon icon={eyeIcon} onPress={showPass}/>}/>
+                    <Button style={{alignSelf:'flex-end'}}>Forgot password?</Button>
                     <Button mode='contained'>Log-in</Button>
-                    <Button>Sign-up</Button>
+                    <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+                        <Text>Don't have an account?</Text>
+                        <Button>Sign-up</Button>
+                    </View>
                 </Card.Content>
             </Card>
         </View>
@@ -27,7 +42,6 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: 'center',
         alignItems:'center',
-        flexDirection:'row',
     },
     view:{
         width:'80%'
