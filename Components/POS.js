038d16c7e6b8c452c3710769dd66c4f69ff7d1ce
Niel Pagupat <niel.pagupat.2016@gmail.com>
2023-12-29 11:40:21 +0800

    gamay rajud ni na add

--- Components/POS.js
+++ Components/POS.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Card, Button, TextInput, Portal, Modal} from 'react-native-paper'
@@ -17,14 +17,13 @@ export default function POS() {
     const {email} = route.params
     const {userID} = route.params
     const [total, setTotal] = useState('0');
-    const [bcNum, setBcNum] = useState('');
+    const [ctotal, setCtotal] = useState('0');
     
     const [code, setCode] = useState('')
     const [multiplier, setMultiplier] = useState('1')
     const [isActiveBarcodeSearch, setBarcodeSearch] = useState(false)
     const [isActiveMultiplier, setActiveMultiplier] = useState(false)
-    const [clicked, setClick] = useState('#987554')
-    const [mclicked, setMclicked] = useState('#987554')
+    
     
     const [productSold, setProductSold] = useState([])
 
@@ -56,15 +55,18 @@ export default function POS() {
           'PID': data,
           'UID': userID
         }})
-        // Add product to sold list 
-        const arr = productSold
-        const value =  parseInt(total) + parseInt(result.data.price[0].retail_price)
+        
+        const product = parseInt(result.data.price[0].retail_price)*parseInt(multiplier)
+        const value =  parseInt(total) + parseInt(product)
         setTotal(value.toString())
         setScanned(true)
 
+        // Add product to sold list 
+        const arr = productSold
         arr.push({
           "PID": code,
           "UID": userID,
+          "name": result.data.price[0].product_name,
           "Price": result.data.price[0].retail_price,
           "Quantity": multiplier,
         })
@@ -74,26 +76,42 @@ export default function POS() {
       };
 
       const reset = async() =>{
+        console.log(code, userID)
+
         const result = await axios.get(Link('/getPrice'), {params:{
           'PID': code,
           'UID': userID
         }})
+
         if (isActiveBarcodeSearch == true) {
-          const productprice = result.data.price[0].retail_price
-          console.log(result.data)
-          if (isActiveMultiplier == true) {
-            const product = parseInt(productprice)*parseInt(multiplier)
-            const value = parseInt(total) + parseInt(product)
-            setTotal(value.toString())
-            setScanned(true)
-            setActiveMultiplier(false)
-          } else {
-            const value =  parseInt(total) + parseInt(result.data.price[0].retail_price)
-            setTotal(value.toString())
-            setScanned(true)
-          }
+          const product = parseInt(result.data.price[0].retail_price)*parseInt(multiplier)
+          const capital = parseInt(result.data.price[0].capital_price)*parseInt(multiplier)
+          const value =  parseInt(total) + parseInt(product)
+          const cValue = parseInt(ctotal) + parseInt(capital)
+          setCtotal(cValue.toString())
+          setTotal(value.toString())
+          setBarcodeSearch(false)
+          setScanned(false)
+          setCode('')
+          setMultiplier('1')
+
+          const arr = productSold
+          arr.push({
+          "PID": code,
+          "UID": userID,
+          "name": result.data.price[0].product_name,
+          "capital": result.data.price[0].capital_price,
+          "Price": result.data.price[0].retail_price,
+          "Quantity": multiplier,
+        })
+
+          setProductSold(arr)
+
+
         } else {
           setScanned(false)
+          setCode('')
+          setMultiplier('1')
         }
         
       }
@@ -112,36 +130,21 @@ export default function POS() {
       const clear = () => {
         setTotal('0')
         setCode('')
-        setMultiplier('')
+        setMultiplier('1')
+        setProductSold([])
+        setCtotal('0')
+        console.log(productSold, ctotal)
       }
 
-      
-      const getBarcode = () =>{
-        if (isActiveBarcodeSearch == false) {
-          setClick('#9c8c7c')
-          setBarcodeSearch(true)
-        } else {
-          setClick('#987554')
-          setBarcodeSearch(false)
-        }
-      }
-
-      const multiplierStatus = () => {
-        if (isActiveMultiplier == false) {
-          setMclicked('#9c8c7c')
-          setActiveMultiplier(true)
-        } else {
-          setMclicked('#987554')
-          setActiveMultiplier(false)
-        }
-      }
       const numpadInput = (value) => {
         if (isActiveBarcodeSearch == true) {
           setCode(code+value)
         }
-        if (isActiveMultiplier == true) {
-          setMultiplier(multiplier+value)
-        }
+      }
+        
+
+      const multiplierInput = (val) =>{
+        setMultiplier(multiplier+val)
       }
      
     const showBarcodeModal = () => {
@@ -150,12 +153,12 @@ export default function POS() {
     }
     const hideBarcodeModal = () => {
       SetBarcodeVisible(false);
-      setBarcodeSearch(false);
     }
     const barcodeContainerStyle = {backgroundColor: '#987554', height: 370, margin: 40, borderRadius: 10, padding:10};
 
     const showMultiplyModal = () => {
       SetMultiplyVisible(true);
+      setMultiplier('')
       setActiveMultiplier(true);
     }
     const hideMultiplyModal = () => {
@@ -182,7 +185,21 @@ export default function POS() {
                     <View style={{flex:1, justifyContent:'center'}}>
                         <View style={{flex:3, flexDirection:'row', justifyContent:'center'}}>
                             <View style={{flex:3, marginBottom:5, borderRadius:5, backgroundColor:'#D9D9D9'}}>
-                              <Text>PENIS</Text>
+                                <View style={{flexDirection:'row', justifyContent:'space-around'}}> 
+                                    <Text>Name</Text>
+                                    <Text>Price</Text>
+                                    <Text>Quantity</Text>
+                                </View>
+                              <ScrollView>
+                                  {
+                                    productSold.map((obj, i)=>
+                                    <View key={i}style={{flexDirection:'row', justifyContent:'space-around'}}>
+                                      <Text>{obj.name}</Text>
+                                      <Text>{obj.Price}</Text>
+                                      <Text>{obj.Quantity}</Text>
+                                    </View>)
+                                  }
+                              </ScrollView>
                             </View>
                             <View style={{flex:2, marginBottom:5, justifyContent:'space-around'}}>
                               <TextInput style={{height:50, marginLeft:5, borderRadius:5, backgroundColor:'#D9D9D9'}} placeholder='Barcode #' underlineColor='transparent' readOnly value={code}/>
@@ -199,7 +216,7 @@ export default function POS() {
                               <FontAwesomeIcon style={{color:'#F5F5F5'}} icon={faX}/>
                               <Text style={{color:'#F5F5F5'}}>Multiply</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{flex:1, flexDirection:'row', backgroundColor:'#987554', height:35, borderRadius:5, alignItems:'center', justifyContent:'space-evenly', marginHorizontal:2}} onPress={clear}>
+                            <TouchableOpacity style={{flex:1, flexDirection:'row', backgroundColor:'#987554', height:35, borderRadius:5, alignItems:'center', justifyContent:'space-evenly', marginHorizontal:2}} onPress={reset}>
                               <FontAwesomeIcon style={{transform:[{rotate: '90deg'}], color:'#F5F5F5'}} icon={faTurnUp} />
                               <Text style={{color:'#F5F5F5'}}>Next</Text>
                             </TouchableOpacity>
@@ -267,40 +284,40 @@ export default function POS() {
                               </TouchableOpacity>
                               <TextInput style={{marginVertical:4, marginHorizontal:6, borderRadius:5}} underlineColor='transparent' editable={false} value={multiplier}/>
                               <View style={{flexDirection:'row', margin:2}}>
-                                <TouchableOpacity onPress={()=>numpadInput(1)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(1)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>1</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(2)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(2)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>2</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(3)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(3)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>3</Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={{flexDirection:'row', margin:2}}>
-                                <TouchableOpacity onPress={()=>numpadInput(4)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(4)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>4</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(5)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(5)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>5</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(6)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(6)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>6</Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={{flexDirection:'row', margin:2}}>
-                                <TouchableOpacity onPress={()=>numpadInput(7)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(7)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>7</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(8)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(8)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>8</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity onPress={()=>numpadInput(9)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(9)} style={{flex:1, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>9</Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={{flexDirection:'row', margin:2, alignItems:'center'}}>
-                                <TouchableOpacity onPress={()=>numpadInput(0)} style={{flex:2.05, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
+                                <TouchableOpacity onPress={()=>multiplierInput(0)} style={{flex:2.05, backgroundColor:'#CDCEC9', marginHorizontal:2, height:50, alignItems:'center', borderRadius: 5}}>
                                   <Text>0</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={{flexDirection:'row', marginHorizontal:2, alignItems:'center', flex:1, backgroundColor:'#E5D3B3', height:50, borderRadius:5, justifyContent:'space-evenly'}} onPress={hideBarcodeModal}>
